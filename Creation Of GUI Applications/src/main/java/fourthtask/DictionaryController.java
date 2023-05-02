2d475a8215575ba35833f83cd545c57592b2aeab
Nicel193 <64494378+Nicel193@users.noreply.github.com>
2023-05-02 23:37:34 +0300

    Add option 'Add words to TableView'

--- Creation Of GUI Applications/src/main/java/fourthtask/DictionaryController.java
+++ Creation Of GUI Applications/src/main/java/fourthtask/DictionaryController.java
@@ -1,25 +1,35 @@
 package fourthtask;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+
 import java.util.HashMap;
 import java.util.Map;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+
+import javafx.scene.control.TableView;
 
 public class DictionaryController extends Application {
 
     private Map<String, String> dictionary = new HashMap<>();
 
     // Остальной код
-
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/Dictionary.fxml"));
-        Scene scene = new Scene(root);
+        Pane root = (Pane)FXMLLoader.load(getClass().getResource("/DictionaryD.fxml"));
+        Scene scene = new Scene(root, 580, 500);
         stage.setScene(scene);
+        stage.setTitle("Переписи населення");
         stage.show();
     }
 
@@ -27,4 +37,44 @@ public class DictionaryController extends Application {
         launch(args);
     }
 
+    @FXML
+    private TableView<Map.Entry<String, String>> DictionaryTable;
+
+    private ObservableList<Map.Entry<String, String>> tableData;
+
+    @FXML
+    private  TableColumn<String, String> uaColumn;
+
+    @FXML
+    private  TableColumn<String, String> enColumn;
+
+    @FXML
+    public TextField ua;
+
+    @FXML
+    public TextField en;
+
+    @FXML
+    public void initialize() {
+        // Set up the table columns
+        TableColumn<Map.Entry<String, String>, String> uaCol = new TableColumn<>("Українська");
+        uaCol.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getKey()));
+
+        TableColumn<Map.Entry<String, String>, String> enCol = new TableColumn<>("English");
+        enCol.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getValue()));
+
+        DictionaryTable.getColumns().addAll(uaCol, enCol);
+
+        // Set up the table data
+        tableData = FXCollections.observableArrayList(dictionary.entrySet());
+        DictionaryTable.setItems(tableData);
+    }
+
+    public void AddWords(javafx.event.ActionEvent actionEvent) {
+        // Add the new word to the dictionary
+        dictionary.put(ua.getText(), en.getText());
+
+        // Update the table data
+        tableData.setAll(dictionary.entrySet());
+    }
 }
